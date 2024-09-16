ddc646b2a6c8f6391be4b04da03369b4f55307bf
Brooks Mershon <brooks.mershon@earthscope.org>
2024-09-16 10:39:46 -0700

    add favicon

--- observablehq.config.js
+++ observablehq.config.js
@@ -17,7 +17,8 @@ export default {
   // ],
 
   // Content to add to the head of the page, e.g. for a favicon:
-  head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
+  head: () =>
+    `<link rel="icon" href="/cropped-favicon-32x32.png" type="image/png" sizes="32x32">`,
 
   // The path to the source root.
   root: "src",
